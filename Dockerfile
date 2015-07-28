ee5e72452995f75477e9f3aa7042a87da60be5b0
Luke Roberts <luke@sqwiggle.com>
2015-07-28 17:38:08 +0200

    dummy rabbit consumer

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Start from a Debian image with the latest version of Go installed
+# and a workspace (GOPATH) configured at /go.
+FROM golang:latest
+
+#Cache dependencies
+RUN go get	"github.com/streadway/amqp"
+
+# Copy the local package files to the container's workspace.
+ADD . /go/src/github.com/thethingsnetwork/jolie
+WORKDIR /go/src/github.com/thethingsnetwork/jolie
+
+RUN go build .
+
+CMD ["./jolie"]
+
+
---
+++ rabbit.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+	"os"
+	"time"
+)
+
+func EnsureRabbitConnection() (*amqp.Connection, error) {
+	// Connects opens an AMQP connection from the credentials in the URL.
+	var err error
+	for i := 0; i < 20; i++ {
+		conn, err := amqp.Dial(os.Getenv("AMQP_URI"))
+		if err != nil {
+			log.Print("Couldn't get rabbit connection")
+			log.Print(err.Error())
+			time.Sleep(time.Duration(2) * time.Second)
+			log.Print("Retrying.....")
+		} else {
+			return conn, nil
+		}
+	}
+	log.Print("Got rabbit connection")
+	return nil, err
+}
